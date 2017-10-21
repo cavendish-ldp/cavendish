abf023af3c00fc75b7858a57c02b88b34257bead
Benjamin Armintor <armintor@gmail.com>
2017-10-21 16:07:50 -0400

    first iteration test for creating redirect ldp NonRDFSources

--- cavendish-jetty/src/test/java/cavendish/jetty/NonRDFTestCase.java
+++ cavendish-jetty/src/test/java/cavendish/jetty/NonRDFTestCase.java
@@ -2,6 +2,9 @@ package cavendish.jetty;
 
 import static org.junit.Assert.fail;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
 
 import javax.ws.rs.core.HttpHeaders;
@@ -17,9 +20,11 @@ import org.w3.ldp.testsuite.matcher.HttpStatusSuccessMatcher;
 
 import com.jayway.restassured.RestAssured;
 import com.jayway.restassured.response.Header;
+import com.jayway.restassured.response.Response;
 
 import cavendish.ldp.impl.HeaderValue;
 import cavendish.ldp.api.LdpHeaders;
+import cavendish.ldp.api.Vocabulary;
 
 public class NonRDFTestCase extends BaseTestCase {
   private static Logger LOG = LoggerFactory.getLogger(NonRDFTestCase.class);
@@ -67,5 +72,28 @@ public class NonRDFTestCase extends BaseTestCase {
       }
     }
     fail("Expected at least one Accept-Post values for message/external-body");
+  }
+  @Test
+  public void testCreateNonRdfSource() throws IOException {
+    String testTextContent = "test text content";
+    File tempFile = File.createTempFile("testCreateNonRdfSource", "txt");
+    try(FileWriter writer = new FileWriter(tempFile)) {
+      writer.write(testTextContent);
+    }
+    Response response = RestAssured
+        .with().header(HttpHeaders.LINK, Vocabulary.NON_RDF_SOURCE.toString() + ";rel=type")
+        .with().header(HttpHeaders.CONTENT_TYPE, "text/plain")
+        .with().header(HttpHeaders.CONTENT_TYPE, "message/external-body;access-type=URL;URL=" + tempFile.toURI())
+        .expect()
+        .statusCode(HttpStatusSuccessMatcher.isSuccessful())
+        .post(containerUrl);
+    String createdResource = response.getHeader(HttpHeaders.LOCATION);
+    RestAssured.reset();
+    RestAssured.expect()
+        .statusCode(HttpStatusSuccessMatcher.isSuccessful())
+        .header(HttpHeaders.LINK, Link.fromUri(cavendish.ldp.api.Vocabulary.NON_RDF_SOURCE).rel(cavendish.ldp.api.Link.REL_TYPE).build().toString())
+        .header(HttpHeaders.LOCATION, tempFile.toURI().toString())
+        .header(HttpHeaders.CONTENT_LENGTH, Integer.toString(testTextContent.getBytes().length))
+        .get(createdResource);
   }
 }
